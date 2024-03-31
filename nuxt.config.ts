0833d11c3a94507661b85be3593b678a1f5c57fb
Taiga Sato <tiger.tiger.1223@gmail.com>
2024-03-31 19:20:08 +0900

    update src directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@ export default defineNuxtConfig({
   srcDir: "src/",
   app: {
     baseURL: '/scripts-showcase/',
-    // pageTransition: { name: 'page', mode: 'out-in' },
   },
   modules: [
     '@nuxtjs/tailwindcss',
